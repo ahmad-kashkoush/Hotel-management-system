1949eb9f4c0b1204d42aa8b635b6476623334752
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-07-16 17:41:31 +0300

    chore: setup project
    - install dependencies
    - setup project routes in App.jsx
    - update src alias in vite.config.js,

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Account from "@/pages/Account";
+import Bookings from "@/pages/Bookings";
+import Cabins from "@/pages/Cabins";
+import Dashboard from "@/pages/Dashboard";
+import Login from "@/pages/Login";
+import PageNotFound from "@/pages/PageNotFound";
+import Settings from "@/pages/Settings";
+import NewUsers from "@/pages/Users";
+
+import AppLayout from "@/ui/AppLayout";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    path: "/",
+    errorElement: <PageNotFound />,
+    children: [
+      // default
+      { index: true, element: <Navigate to="/dashboard" replace /> },
+      // main page
+      { path: "/dashboard", element: <Dashboard /> },
+      // bookings
+      { path: "/bookings", element: <Bookings /> },
+      { path: "/bookings/:id", element: <Bookings /> },
+      // Cabins
+      { path: "/cabins", element: <Cabins /> },
+      { path: "/cabins/:id", element: <Cabins /> },
+      // users
+      { path: "/users", element: <NewUsers /> },
+      // Authentication
+      { path: "/account", element: <Account /> },
+      // settings
+      { path: "/settings", element: <Settings /> },
+      // settings
+    ],
+  },
+  { path: "/login", element: <Login /> },
+]);
 
 function App() {
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
