573b2e34181d9102954f37573b60afbb8c4b0c96
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-07-19 12:33:56 +0300

    feat(apiCabins): add deleteCabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,3 +11,17 @@ export async function getCabins() {
         console.error(`getCabins(): ${err}`);
     }
 }
+
+export async function deleteCabin(cabinId) {
+    try {
+
+        const { error } = await supabase
+            .from('cabins')
+            .delete()
+            .eq('id', cabinId);
+        if (error) throw error;
+
+    } catch (err) {
+        console.error(`deleteCabin(): ${err}`);
+    }
+}
