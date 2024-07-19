83711dd268be91b2e8a17fbe8954e44a2138f469
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-07-19 12:46:57 +0300

    feat: add toaster

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import Settings from "@/pages/Settings";
 import NewUsers from "@/pages/Users";
 
 import AppLayout from "@/ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 import {
   createBrowserRouter,
   Navigate,
@@ -41,9 +44,36 @@ const router = createBrowserRouter([
   },
   { path: "/login", element: <Login /> },
 ]);
-
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RouterProvider router={router} />;
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: { duration: 3000 },
+          error: { duration: 5000 },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
