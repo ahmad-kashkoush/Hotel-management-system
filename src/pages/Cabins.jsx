da4f352d11992dd1324195fdab5aaba8299f00a5
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-07-25 22:59:53 +0300

    chore: move addCabin to src/features/cabins

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,16 +1,7 @@
-import { useState } from "react";
+import AddCabin from "@/features/cabins/AddCabin";
 import CabinTable from "@/features/cabins/CabinTable";
-import CreateCabinForm from "@/features/cabins/CreateCabinForm";
-import styled from "styled-components";
-import { Button, Heading, Row } from "@/ui";
-import AddCabin from "@/pages/AddCabin";
+import { Heading, Row } from "@/ui";
 
-const Overlay = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  translate: -50% -50%;
-`;
 function Cabins() {
   return (
     <Row>
