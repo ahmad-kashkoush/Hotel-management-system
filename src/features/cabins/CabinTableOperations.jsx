5aa2d8e25a4df8aefee6c7e1e67f7f507d3827c2
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-07-29 22:39:58 +0300

    feat: Create sorting cabins
    - Create SortBy, Select components
    - Call SortyBy in CabinTableOperatiosn
    - Add SortBy to folder exporter(index.js)
    - sort cabins based on sorting state stored in url using searchParams

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,4 +1,4 @@
-import { Filter, TableOperations } from "@/ui";
+import { Filter, SortBy, TableOperations } from "@/ui";
 
 function CabinTableOperations() {
   return (
@@ -9,6 +9,16 @@ function CabinTableOperations() {
           { value: "all", label: "All" },
           { value: "with_discount", label: "With discount" },
           { value: "without_discount", label: "Without discount" },
+        ]}
+      />
+      <SortBy
+        options={[
+          { value: "name-asc", label: "Sort by name (A-Z)" },
+          { value: "name-desc", label: "Sort by name (Z-A)" },
+          { value: "regularprice-asc", label: "Sort by price (Low first)" },
+          { value: "regularprice-desc", label: "Sort by price (High first)" },
+          { value: "maxcapacity-asc", label: "Sort by capacity (Low first)" },
+          { value: "maxcapacity-desc", label: "Sort by capacity (High first)" },
         ]}
       />
     </TableOperations>
