2d2a0cb968565073132b852ca4c4ebede7f5c5ea
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2025-01-26 13:54:15 +0200

    feat: add settings endpoint to client

--- Client/src/services/apiSettings.js
+++ Client/src/services/apiSettings.js
@@ -1,7 +1,10 @@
-import supabase from "./supabase";
+const url = import.meta.env.VITE_ENDPOINT_URL;
+
 
 export async function getSettings() {
-  const { data, error } = await supabase.from("settings").select("*").single();
+  const response = await fetch(`${url}/settings`);
+  const { data, error } = await response.json();
+  // const { data, error } = await supabase.from("settings").select("*").single();
 
   if (error) {
     console.error(error);
@@ -12,12 +15,21 @@ export async function getSettings() {
 
 // We expect a newSetting object that looks like {setting: newValue}
 export async function updateSetting(newSetting) {
-  const { data, error } = await supabase
-    .from("settings")
-    .update(newSetting)
-    // There is only ONE row of settings, and it has the ID=1, and so this is the updated one
-    .eq("id", newSetting.id)
-    .single();
+  const response = await fetch(`${url}/settings`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newSetting),
+  });
+  const {data, error}=await response.json();
+
+  // const { data, error } = await supabase
+  //   .from("settings")
+  //   .update(newSetting)
+  //   // There is only ONE row of settings, and it has the ID=1, and so this is the updated one
+  //   .eq("id", newSetting.id)
+  //   .single();
 
   if (error) {
     console.error(error);
